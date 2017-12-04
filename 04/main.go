5e7ec80439f547326c35ab9a408e83756fb5a1ba
Andrew Mason <andrew@fixedpoint.xyz>
2017-12-04 08:32:29 -0800

    Day 4, part 2

--- 04/main.go
+++ 04/main.go
@@ -7,7 +7,36 @@ import (
 	"strings"
 )
 
-func isValidPassphrase(passphrase string) bool {
+type LetterCount map[rune]int
+
+func (lc LetterCount) isEqual(other LetterCount) bool {
+	if lc == nil && other == nil {
+		return true
+	}
+	if (lc != nil) != (other != nil) {
+		return false
+	}
+	if len(lc) != len(other) {
+		return false
+	}
+
+	for k, v := range lc {
+		if v2, ok := other[k]; !ok || v != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+func count(word string) LetterCount {
+	counter := make(map[rune]int, len(word))
+	for _, c := range word {
+		counter[c]++
+	}
+	return counter
+}
+
+func isValidPassphrase1(passphrase string) bool {
 	words := strings.Fields(passphrase)
 	cache := make(map[string]bool, len(words))
 	for _, word := range words {
@@ -19,6 +48,22 @@ func isValidPassphrase(passphrase string) bool {
 	return true
 }
 
+func isValidPassphrase2(passphrase string) bool {
+	words := strings.Fields(passphrase)
+	for i, word := range words {
+		counter := count(word)
+		for j, otherWord := range words {
+			if i == j {
+				continue
+			}
+			if word == otherWord || counter.isEqual(count(otherWord)) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func main() {
 	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
@@ -26,11 +71,15 @@ func main() {
 	}
 
 	lines := strings.FieldsFunc(string(input), func(r rune) bool { return r == '\n' })
-	valid := 0
+	valid1, valid2 := 0, 0
 	for _, line := range lines {
-		if isValidPassphrase(line) {
-			valid++
+		if isValidPassphrase1(line) {
+			valid1++
+		}
+		if isValidPassphrase2(line) {
+			valid2++
 		}
 	}
-	fmt.Println(valid)
+	fmt.Println(valid1)
+	fmt.Println(valid2)
 }
