29fcbb8450c89ae1f5b409c44a46af27e4558344
Andrew Mason <andrew@fixedpoint.xyz>
2017-12-06 06:02:46 -0800

    Add struct and iteration logic

--- 06/main.go
+++ 06/main.go
@@ -8,6 +8,49 @@ import (
 	"strings"
 )
 
+type MemoryBank struct {
+	banks      []int
+	Iterations int
+}
+
+func NewMemoryBank(banks []int) *MemoryBank {
+	return &MemoryBank{
+		banks:      banks,
+		Iterations: 0,
+	}
+}
+
+func (m *MemoryBank) Next() *MemoryBank {
+	i := findMax(m.banks)
+	redistributionAmount := m.banks[i]
+	newBanks := append([]int(nil), m.banks...)
+	newBanks[i] = 0
+	for redistributionAmount > 0 {
+		i = (i + 1) % len(newBanks)
+		newBanks[i]++
+		redistributionAmount--
+	}
+	return &MemoryBank{
+		banks:      newBanks,
+		Iterations: m.Iterations + 1,
+	}
+}
+
+func findMax(arr []int) (index int) {
+	if len(arr) == 0 {
+		return -1
+	}
+	index = 0
+	max := arr[index]
+	for i, v := range arr {
+		if v > max {
+			max = v
+			index = i
+		}
+	}
+	return
+}
+
 func readBanks(input string) (banks []int, err error) {
 	fields := strings.Fields(input)
 	banks = make([]int, 0, len(fields))
