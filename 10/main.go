979630c65ea3b9642d50047d7851d65373e61e4e
Andrew Mason <andrew@fixedpoint.xyz>
2017-12-14 05:48:56 -0800

    Move hashknot to a util package

--- 10/main.go
+++ 10/main.go
@@ -8,78 +8,9 @@ import (
 	"strings"
 )
 
-const (
-	KNOT_SIZE  = 256
-	NUM_ROUNDS = 64
-	BLOCK_SIZE = 16
+import (
+	"github.com/ajm188/advent-of-code-17/util"
 )
-
-type HashKnot struct {
-	index    int
-	skipSize int
-	ring     []int
-}
-
-func NewHashKnot() *HashKnot {
-	numbers := make([]int, KNOT_SIZE)
-	i := 0
-	for i < len(numbers) {
-		numbers[i] = i
-		i++
-	}
-	return &HashKnot{
-		index:    0,
-		skipSize: 0,
-		ring:     numbers,
-	}
-}
-
-func (hk *HashKnot) Hash() []int {
-	numBlocks := len(hk.ring) / BLOCK_SIZE
-	blocks := make([]int, 0, numBlocks)
-	i := 0
-	for i < numBlocks {
-		start := i * BLOCK_SIZE
-		end := start + BLOCK_SIZE
-		block := hk.ring[start:end]
-
-		v := block[0]
-		j := 1
-		for j < BLOCK_SIZE {
-			v ^= block[j]
-			j++
-		}
-		blocks = append(blocks, v)
-		i++
-	}
-	return blocks
-}
-
-func (hk *HashKnot) RunRound(lengths []int) {
-	for _, length := range lengths {
-		hk.PinchTwist(length)
-	}
-}
-
-func (hk *HashKnot) PinchTwist(length int) {
-	if length > len(hk.ring) {
-		return
-	}
-
-	buf := make([]int, length)
-	for j, _ := range buf {
-		i := ((hk.index + length) - j - 1) % len(hk.ring)
-		buf[j] = hk.ring[i]
-	}
-
-	for i, v := range buf {
-		j := (hk.index + i) % len(hk.ring)
-		hk.ring[j] = v
-	}
-
-	hk.index = (hk.index + hk.skipSize + length) % len(hk.ring)
-	hk.skipSize++
-}
 
 func readLengths(str string, f func(rune) bool) ([]int, error) {
 	fields := strings.FieldsFunc(str, f)
@@ -116,18 +47,18 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	hk := NewHashKnot()
+	hk := util.NewHashKnot()
 	hk.RunRound(lengths)
 
-	fmt.Println(hk.ring[0] * hk.ring[1])
+	fmt.Println(hk.Ring[0] * hk.Ring[1])
 
-	hk = NewHashKnot()
+	hk = util.NewHashKnot()
 	lengths = append(
 		asASCIICodes(string(input[0:len(input)-1])),
 		[]int{17, 31, 73, 47, 23}...,
 	)
 	i := 0
-	for i < NUM_ROUNDS {
+	for i < util.NUM_ROUNDS {
 		hk.RunRound(lengths)
 		i++
 	}
---
+++ util/hash_knot.go
@@ -0,0 +1,74 @@
+package util
+
+const (
+	KNOT_SIZE  = 256
+	NUM_ROUNDS = 64
+	BLOCK_SIZE = 16
+)
+
+type HashKnot struct {
+	index    int
+	skipSize int
+	Ring     []int
+}
+
+func NewHashKnot() *HashKnot {
+	numbers := make([]int, KNOT_SIZE)
+	i := 0
+	for i < len(numbers) {
+		numbers[i] = i
+		i++
+	}
+	return &HashKnot{
+		index:    0,
+		skipSize: 0,
+		Ring:     numbers,
+	}
+}
+
+func (hk *HashKnot) Hash() []int {
+	numBlocks := len(hk.Ring) / BLOCK_SIZE
+	blocks := make([]int, 0, numBlocks)
+	i := 0
+	for i < numBlocks {
+		start := i * BLOCK_SIZE
+		end := start + BLOCK_SIZE
+		block := hk.Ring[start:end]
+
+		v := block[0]
+		j := 1
+		for j < BLOCK_SIZE {
+			v ^= block[j]
+			j++
+		}
+		blocks = append(blocks, v)
+		i++
+	}
+	return blocks
+}
+
+func (hk *HashKnot) RunRound(lengths []int) {
+	for _, length := range lengths {
+		hk.PinchTwist(length)
+	}
+}
+
+func (hk *HashKnot) PinchTwist(length int) {
+	if length > len(hk.Ring) {
+		return
+	}
+
+	buf := make([]int, length)
+	for j, _ := range buf {
+		i := ((hk.index + length) - j - 1) % len(hk.Ring)
+		buf[j] = hk.Ring[i]
+	}
+
+	for i, v := range buf {
+		j := (hk.index + i) % len(hk.Ring)
+		hk.Ring[j] = v
+	}
+
+	hk.index = (hk.index + hk.skipSize + length) % len(hk.Ring)
+	hk.skipSize++
+}
