a474b6e1f0ab7f646f5953b31037c00a1800f3f0
Andrew Mason <andrew@fixedpoint.xyz>
2017-12-14 10:00:52 -0800

    Part 2 done

--- 14/main.go
+++ 14/main.go
@@ -14,8 +14,73 @@ const (
 	INPUT = "ljoxqyyw"
 )
 
+type Point struct {
+	r, c int
+}
+
+func (p Point) Neighbors() []Point {
+	return []Point{
+		Point{p.r + 1, p.c},
+		Point{p.r - 1, p.c},
+		Point{p.r, p.c + 1},
+		Point{p.r, p.c - 1},
+	}
+}
+
+func (p Point) InRange(grid []string) bool {
+	return p.r >= 0 && p.c >= 0 && p.r < len(grid) && p.c < len(grid[p.r])
+}
+
+func findRegions(grid []string) map[int][]Point {
+	pointMap := make(map[Point]int, len(grid)*len(grid))
+	regionMap := make(map[int][]Point, 0)
+	region := 1
+	for r := 0; r < len(grid); r++ {
+		for c := 0; c < len(grid[r]); c++ {
+			p := Point{r, c}
+			if _, hasRegion := pointMap[p]; hasRegion {
+				continue
+			}
+			if grid[r][c] == '0' {
+				pointMap[p] = -1 // space is free, no region
+			} else {
+				points := discoverRegion(grid, p, pointMap, region)
+				regionMap[region] = points
+				region++
+			}
+		}
+	}
+	return regionMap
+}
+
+func discoverRegion(grid []string, point Point, pointMap map[Point]int, region int) []Point {
+	points := make([]Point, 0)
+	queue := []Point{point}
+	for len(queue) > 0 {
+		point, queue = queue[0], queue[1:]
+		if _, hasRegion := pointMap[point]; hasRegion {
+			// point already assigned to region, stop searching
+			continue
+		}
+		if !point.InRange(grid) {
+			// point is out of bounds, skip
+			continue
+		}
+		if grid[point.r][point.c] == '0' {
+			// region border found, stop searching
+			pointMap[point] = -1
+			continue
+		}
+		queue = append(queue, point.Neighbors()...)
+		points = append(points, point)
+		pointMap[point] = region
+	}
+	return points
+}
+
 func main() {
 	ones := 0
+	grid := make([]string, 128)
 	for i := 0; i < 128; i++ {
 		codes := util.AsASCIICodes(fmt.Sprintf("%s-%d", INPUT, i))
 		hk := util.NewHashKnot()
@@ -41,7 +106,10 @@ func main() {
 				row += bin
 			}
 		}
+		grid[i] = row
 		ones += strings.Count(row, "1")
 	}
 	fmt.Println(ones)
+	regions := findRegions(grid)
+	fmt.Println(len(regions))
 }
